fa4e7a5f239b023749117a18b05a5a80a55a2a61
zero4994 <cavc94@gmail.com>
2019-04-15 16:57:57 +0900

    Adds tests and implementation for algorithm

--- src/meetup-1/palindrome.js
+++ src/meetup-1/palindrome.js
@@ -1,5 +1,14 @@
-const palindrome = () => {
-  console.log("Hello World");
+/*
+Write a function that checks if two strings are palindromes of each other.
+*/
+
+const isPalindrome = (a, b) => {
+  const reversed = a
+    .split("")
+    .reverse()
+    .join("");
+
+  return reversed === b;
 };
 
-module.exports = { palindrome };
+module.exports = { isPalindrome };
--- test/palindrome.js
+++ test/palindrome.js
@@ -1,8 +1,25 @@
 /* eslint-disable no-unused-expressions */
 const { expect } = require("chai");
+const { isPalindrome } = require("../src/meetup-1/palindrome");
 
 describe("palindromes", () => {
   it("should be able to determine if two strings are palindromes of each other", () => {
-    expect(false).to.be.true;
+    const string1 = "anita lava la tina";
+    const string2 = "anit al aval atina";
+
+    expect(isPalindrome(string1, string2)).to.be.true;
+    expect(isPalindrome(string2, string1)).to.be.true;
+  });
+
+  it("should work with other palindromes", () => {
+    const string1 = "A Man, A Plan, A Canal-Panama";
+    const string2 = "amanaP-lanaC A ,nalP A ,naM A";
+    expect(isPalindrome(string1, string2)).to.be.true;
+  });
+
+  it("should return false when the strings provided are not palindromes", () => {
+    const string1 = "This is a string";
+    const string2 = "This is another string";
+    expect(isPalindrome(string1, string2)).to.be.false;
   });
 });
