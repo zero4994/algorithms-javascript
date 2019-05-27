5a6660279e76ceb19ce131eecfe7a5762ee30e17
zero4994 <cavc94@gmail.com>
2019-05-27 17:45:47 +0900

    Adds code to pass test

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   env: {
     es6: true,
     node: true,
-    mocha: true
+    mocha: true,
+    browser: true
   },
   extends: "airbnb-base",
   rules: { quotes: [2, "double", "avoid-escape"] }
--- src/meetup-1/4-hills.js
+++ src/meetup-1/4-hills.js
@@ -1,3 +1,21 @@
-const isRoutePosible = () => {};
+const isRoutePosible = (n, bridges) => {
+  const start = bridges[0][0];
+  let isPriceReached = false;
+  let hasReachedStart = false;
+
+  for (let i = 1; i < bridges.length; i++) {
+    if (bridges[i - 1][1] !== bridges[i][0]) {
+      return false;
+    } if (bridges[i][1] === start) {
+      hasReachedStart = true;
+    }
+
+    if (bridges[i][1] === n) {
+      isPriceReached = true;
+    }
+  }
+
+  return isPriceReached && hasReachedStart;
+};
 
 module.exports = { isRoutePosible };
